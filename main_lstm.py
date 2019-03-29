bca745ab9b2d00d1a8a407f24d8fc660c95d7e79
Min Cai <min.cai.china@gmail.com>
2019-03-29 15:33:48 +0800

    Add main_lstm.py and main_mlp.py.

---
+++ main_lstm.py
@@ -0,0 +1,10 @@
+from keras.datasets import mnist
+
+from predictor_lstm import LSTMPredictor
+
+if __name__ == '__main__':
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    predictor_lstm = LSTMPredictor(x_train, y_train, x_test, y_test)
+    predictor_lstm.fit()
+    predictor_lstm.evaluate()
--- main_mlp.py
+++ main_mlp.py
@@ -1,6 +1,5 @@
 from keras.datasets import mnist
 
-from predictor_lstm import LSTMPredictor
 from predictor_mlp import MLPPredictor
 
 if __name__ == '__main__':
@@ -9,7 +8,3 @@ if __name__ == '__main__':
     predictor_mlp = MLPPredictor(x_train, y_train, x_test, y_test)
     predictor_mlp.fit()
     predictor_mlp.evaluate()
-
-    predictor_lstm = LSTMPredictor(x_train, y_train, x_test, y_test)
-    predictor_lstm.fit()
-    predictor_lstm.evaluate()
